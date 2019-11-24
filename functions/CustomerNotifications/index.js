bbdf63f7afcfab27452c38ed9e32e8400ac23984
Shekhar Patnaik <patnaikshekhar@gmail.com>
2019-11-24 18:23:23 +0000

    Added BlobTrigger to file

--- functions/CustomerNotifications/index.js
+++ functions/CustomerNotifications/index.js
@@ -1,3 +1,4 @@
-module.exports = async function (context, myBlob) {
+module.exports = async function (context, myBlob, blobValue) {
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+    console.log(blobValue)
 };
