ea6b03f0dc3acbd48ae334560465258696906cc8
Shekhar Patnaik <patnaikshekhar@gmail.com>
2019-11-27 08:52:43 +0000

    Added twilio and send grid integration

--- functions/CustomerNotifications/index.js
+++ functions/CustomerNotifications/index.js
@@ -1,3 +1,10 @@
+const twilioAccountSid = process.env.TWIL_ACCOUNT_ID
+const twilioAuthToken = process.env.TWIL_AUTH_TOKEN
+const twilioSendingNumber = process.env.TWIL_SENDING_NUMBER
+const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken)
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
 module.exports = async function (context, myBlob, blobValue) {
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
     const rows = blobValue.split('\n')
@@ -26,9 +33,34 @@ module.exports = async function (context, myBlob, blobValue) {
 };
 
 async function sendSMS(phoneNumber, message) {
-    console.log('Sending SMS to', phoneNumber)
+    try {
+        console.log('Sending SMS to', phoneNumber)
+        const messageID = await twilioClient.messages.create({
+            body: message,
+            from: twilioSendingNumber,
+            to: phoneNumber
+        })
+        console.log('Twilio Message ID', messageID)
+    } catch(e) {
+        console.error(e)
+    }
 }
 
 async function sendEmail(emailAddress, message) {
-    console.log('Sending an Email to ', emailAddress)
+    try {
+        console.log('Sending an Email to ', emailAddress)
+        const msg = {
+            to: emailAddress,
+            from: 'patnaikshekhar@gmail.com',
+            subject: message,
+            text: message,
+            html: message,
+        }
+
+        const response = await sgMail.send(msg)
+
+        console.log('Response from SendGrid', response)
+    } catch(e) {
+        console.error(e)
+    }
 }
